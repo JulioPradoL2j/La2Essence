68a8c377e68c67a3ebbc469739b07a6aa9cedb67
Julio <juliopradrol2j@gmail.com>
2026-02-26 12:54:33 -0300

    main

--- java/org/l2jmobius/log/formatter/ConsoleLogFormatter.java
+++ java/org/l2jmobius/log/formatter/ConsoleLogFormatter.java
@@ -1,33 +1,30 @@
 package org.l2jmobius.log.formatter;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
-import org.l2jmobius.commons.util.StringUtil;
 import org.l2jmobius.commons.util.TraceUtil;
 
 public class ConsoleLogFormatter extends Formatter
 {
-	private final SimpleDateFormat _dateFormat = new SimpleDateFormat("dd/MM HH:mm:ss");
-
 	@Override
 	public String format(LogRecord record)
 	{
 		StringBuilder output = new StringBuilder(128);
-		StringUtil.append(output, "[", this._dateFormat.format(new Date(record.getMillis())), "] " + record.getMessage(), System.lineSeparator());
+		
+		output.append(record.getLevel().getName()).append(": ").append(record.getMessage()).append(System.lineSeparator());
+		
 		if (record.getThrown() != null)
 		{
 			try
 			{
-				StringUtil.append(output, TraceUtil.getStackTrace(record.getThrown()), System.lineSeparator());
+				output.append(TraceUtil.getStackTrace(record.getThrown())).append(System.lineSeparator());
 			}
-			catch (Exception var4)
+			catch (Exception ignored)
 			{
 			}
 		}
-
+		
 		return output.toString();
 	}
 }
