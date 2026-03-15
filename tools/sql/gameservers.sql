fd9b8f9ca0302d1711edc7979b36b97bac674cbe
Julio <juliopradrol2j@gmail.com>
2026-03-15 07:39:10 -0300

    Fix Register GameServer
    By Vinicios

--- tools/sql/gameservers.sql
+++ tools/sql/gameservers.sql
@@ -1,9 +1,12 @@
 DROP TABLE IF EXISTS `gameservers`;
-CREATE TABLE IF NOT EXISTS `gameservers` (
-  `server_id` int(11) NOT NULL DEFAULT '0',
+CREATE TABLE `gameservers` (
+  `server_id` int(11) NOT NULL DEFAULT 0,
   `hexid` varchar(50) NOT NULL DEFAULT '',
   `host` varchar(50) NOT NULL DEFAULT '',
   PRIMARY KEY (`server_id`)
-) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO `gameservers` VALUES ('1', '-2ad66b3f483c22be097019f55c8abdf0', '');
+-- ----------------------------
+-- Records of gameservers
+-- ----------------------------
+INSERT INTO `gameservers` VALUES ('1', '-7ae81ec60b1c765652518a4dc9058f25', '127.0.0.1');
